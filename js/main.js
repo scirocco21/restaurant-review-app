4b1bf592a0241b8117e9694ea6ee778f0eef6dc5
Sebastian <sebgertz@gmail.com>
2018-12-21 13:08:00 -0500

    accessibility: add aria-label for details view and alt tag for image

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `Photo of restaurant ${restaurant.name}`;
   li.append(image);
 
   const name = document.createElement('h1');
@@ -176,6 +177,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(address);
 
   const more = document.createElement('a');
+  more.setAttribute('aria-label', `More details for restaurant ${restaurant.name}`);
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
