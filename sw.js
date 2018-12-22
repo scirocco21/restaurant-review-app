cba0ee89d7c831be0684510ee4c8588df65d4cf0
Sebastian <sebgertz@gmail.com>
2018-12-22 10:06:52 -0500

    feat: add service worker for offline access

---
+++ sw.js
@@ -0,0 +1,51 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('restaurant-reviews-cache').then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'restaurant.html',
+        'css/styles.css',
+        'js/dbhelper.js',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'data/restaurants.json',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg',
+        'img/10.jpg'
+      ])
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['restaurant-reviews-cache'];
+
+  event.waitUntil(
+    caches.keys(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) == -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);;
+    }).catch(function() {
+      return caches.match('index.html');
+    })
+  );
+});
